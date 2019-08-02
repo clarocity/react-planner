fae1d4cdc94a5da94b9c8dd86d6c4cc4bf871a19
Jocelyn Badgley <jbadgley@clarocity.com>
2019-08-02 11:38:31 -0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      "no-prototype-builtins": 'off',
+      "no-redeclare": 'warn',
+      "no-unused-vars": "warn",
+      "no-useless-escape": "off",
+    },
+    "overrides": [
+      {
+        files: [
+          "**/webpack.config.js",
+        ],
+        "env": {
+          "node": true,
+        },
+        "parserOptions": {
+          "ecmaVersion": 6,
+          "sourceType": "script",
+          "ecmaFeatures": {
+            "jsx": false
+          }
+        },
+      }
+    ]
+};
