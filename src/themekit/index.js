624e92b7195fa6186f07d0a59d510d6e95b0eee5
Jocelyn Badgley <jbadgley@clarocity.com>
2019-11-25 11:30:39 -0800

    Add ability to do concatination with StyleVars

--- src/themekit/index.js
+++ src/themekit/index.js
@@ -61,9 +61,20 @@ export class StyleVar {
   constructor (address, def) {
     this.address = address;
     this.default = def;
+    this.delimiter = ' ';
   }
 
   fetch (tk, key) {
+    if (Array.isArray(this.address)) {
+      return this.address.map((subaddress) => {
+        if (subaddress === key) throw new Error('StyleVar target is same as destination.');
+        if (subaddress instanceof StyleVar) {
+          return subaddress.fetch(tk);
+        }
+        return subaddress;
+      }).join(this.delimiter);
+    }
+
     if (this.address === key) throw new Error('StyleVar target is same as destination.');
     const result = tk.lookup(this.address, this.default);
     if (result instanceof StyleVar) {
